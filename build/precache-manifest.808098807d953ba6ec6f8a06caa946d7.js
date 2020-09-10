4e975cb395e00e67a196c93f8d1a938a6205b622
ashalexis <ashalexis-1297@users.noreply.github.com>
2020-09-10 11:25:12 +1000

    build done

---
+++ build/precache-manifest.808098807d953ba6ec6f8a06caa946d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9118f892005798c432cbb8db28598630",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "89a390106719729f8c8c",
+    "url": "/todo-list/static/css/main.5c862af3.chunk.css"
+  },
+  {
+    "revision": "a276061a34fffe144bd2",
+    "url": "/todo-list/static/js/2.2c262c6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.2c262c6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89a390106719729f8c8c",
+    "url": "/todo-list/static/js/main.82b93cfd.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  }
+]);
